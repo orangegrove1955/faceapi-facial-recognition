e5a88dd294142aa5b024d764bd4cc65973ad5c24
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-06-16 23:20:27 +1000

    Corrections to image display to allow size to be controlled

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const imageUpload = document.getElementById("image-upload");
+const imageWrapper = document.getElementById("image");
 
 const startRecognition = () => {
   document.body.append("Loaded");
   // Add event listener to determine when file is uploaded
   imageUpload.addEventListener("change", async () => {
     const image = await faceapi.bufferToImage(imageUpload.files[0]);
-    document.body.append(image);
+
+    console.log(image);
+    image.style.width = "100%";
+    imageWrapper.append(image);
     const detections = await faceapi
       .detectAllFaces(image)
       .withFaceLandmarks()
