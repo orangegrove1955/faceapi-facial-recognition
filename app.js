95787d5db22b4d0b9986ad8c8e18ab7d3b8c8f6f
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-06-16 23:47:58 +1000

    Added loading feedback

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 const imageUpload = document.getElementById("image-upload");
 const imageWrapper = document.getElementById("image");
+const loader = document.getElementById("loading");
 imageWrapper.style.position = "relative";
 
+/** When files are uploaded, display image and perform detections */
+const onFileUpload = async () => {
+  loader.style.display = "flex";
+  const image = await faceapi.bufferToImage(imageUpload.files[0]);
+
+  image.style.width = "100%";
+  image.style.height = "auto";
+  imageWrapper.append(image);
+
+  // Create canvas to overlay image
+  const canvas = faceapi.createCanvasFromMedia(image);
+  imageWrapper.append(canvas);
+
+  // Change dimensions to overlay correctly
+  const displaySize = { width: image.width, height: image.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  // Get detections from image
+  const detections = await faceapi
+    .detectAllFaces(image)
+    .withFaceLandmarks()
+    .withFaceDescriptors();
+
+  // Resize detections to fit onto canvas, then create box for each
+  const resizedDetections = faceapi.resizeResults(detections, displaySize);
+  resizedDetections.forEach((detection) => {
+    const box = detection.detection.box;
+    const drawBox = new faceapi.draw.DrawBox(box, { label: "Face" });
+    drawBox.draw(canvas);
+  });
+
+  loader.style.display = "none";
+};
+
+/** Start recognition once all models are loaded from faceapi */
 const startRecognition = () => {
   // TODO: Change to loading animation that stopped when ready
-  document.body.append("Loaded");
+  loader.style.display = "none";
 
   // Add event listener to determine when file is uploaded
-  imageUpload.addEventListener("change", async () => {
-    const image = await faceapi.bufferToImage(imageUpload.files[0]);
-
-    image.style.width = "100%";
-    imageWrapper.append(image);
-
-    // Create canvas to overlay image
-    const canvas = faceapi.createCanvasFromMedia(image);
-    imageWrapper.append(canvas);
-
-    // Change dimensions to overlay correctly
-    const displaySize = { width: image.width, height: image.height };
-    faceapi.matchDimensions(canvas, displaySize);
-
-    // Get detections from image
-    const detections = await faceapi
-      .detectAllFaces(image)
-      .withFaceLandmarks()
-      .withFaceDescriptors();
-
-    // Resize detections to fit onto canvas, then create box for each
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    resizedDetections.forEach((detection) => {
-      const box = detection.detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, { label: "Face" });
-      drawBox.draw(canvas);
-    });
-  });
+  imageUpload.addEventListener("change", onFileUpload);
 };
 
 Promise.all([
